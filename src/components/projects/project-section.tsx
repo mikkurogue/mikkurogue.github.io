4ab4f850afeaa8f29e1349a466e8ba8964791a3e
mikkurogue <michael.lindemans@outlook.com>
2026-01-12 13:47:18 +0200

    add jjkk as a project

--- src/components/projects/project-section.tsx
+++ src/components/projects/project-section.tsx
@@ -2,6 +2,15 @@ import ProjectCard, { type Project } from "./project-card";
 
 const projects: Project[] = [
 	{
+		id: "jjkk",
+		name: "jjkk",
+		description: `A minimalistic jj vcs tui tool, akin to lazygit. Simplifies interaction with jj with the bare utilities needed to interact with a jj repository, without having to learn the entire cli.
+      Built with Rust and using ratatui + jj-lib, alongside the jj cli. Slowly working to remove the interop to use the jj-lib as much as possible.`,
+		tags: ["jj", "git", "rust", "cli", "tui"],
+		active: true,
+		url: "https://github.com/mikkurogue/jjkk",
+	},
+	{
 		id: "ts-analyzer",
 		name: "ts-analyzer",
 		description: `
